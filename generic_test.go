fa0238f88d46993caa051be5e70c581348ccbbc9
Tom <tjpalmer@tjpalmer.com>
2020-06-22 18:05:18 -0700

    Use correct n

--- generic_test.go
+++ generic_test.go
@@ -4,40 +4,38 @@ import (
 	"testing"
 )
 
-const count = 1_000_000
-
 func BenchmarkNormNdIVec(b *testing.B) {
-	for i := 0; i < count; i++ {
+	for i := 0; i < b.N; i++ {
 		NormNdIVec(1, 2, 3)
 	}
 }
 
 func BenchmarkNormNd(b *testing.B) {
-	for i := 0; i < count; i++ {
+	for i := 0; i < b.N; i++ {
 		NormNd(1, 2, 3)
 	}
 }
 
 func BenchmarkNorm2fIVec(b *testing.B) {
-	for i := 0; i < count; i++ {
+	for i := 0; i < b.N; i++ {
 		Norm2fIVec(3, 4)
 	}
 }
 
 func BenchmarkNorm2f(b *testing.B) {
-	for i := 0; i < count; i++ {
+	for i := 0; i < b.N; i++ {
 		Norm2f(3, 4)
 	}
 }
 
 func BenchmarkNorm2fSimple(b *testing.B) {
-	for i := 0; i < count; i++ {
+	for i := 0; i < b.N; i++ {
 		Norm2fSimple(3, 4)
 	}
 }
 
 func BenchmarkNorm2fAny(b *testing.B) {
-	for i := 0; i < count; i++ {
+	for i := 0; i < b.N; i++ {
 		Norm2fAny(3, 4)
 	}
 }
